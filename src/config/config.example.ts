a708f4b1a5fde4d1dc8273e68c7fa064798c5f83
Rafal-wq <rafal_wilczewski@wp.pl>
2022-08-28 18:02:53 +0200

    Init TypeOrm settings and DB settings

---
+++ src/config/config.example.ts
@@ -0,0 +1,29 @@
+import {
+    TypeOrmModuleAsyncOptions,
+    TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export class TypeOrmConfig {
+    static getOrmConfig(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '',
+            database: 'name_of_your_database',
+            entities: ['dist/**/**.entity{.ts,.js}'],
+            bigNumberStrings: false,
+            logging: false,
+            synchronize: true, //for development only, not for production mode
+        };
+    }
+}
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    useFactory: async (): Promise<TypeOrmModuleOptions> =>
+        TypeOrmConfig.getOrmConfig(),
+    inject: [ConfigService],
+};
