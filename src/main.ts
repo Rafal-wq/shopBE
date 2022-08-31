1db6899decdfb3e7684c2db7ac1dfd80d4f60a48
Rafal-wq <rafal_wilczewski@wp.pl>
2022-08-31 21:55:24 +0200

    Installed Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  (app as NestExpressApplication).use(helmet);
   await app.listen(3000);
 }
 bootstrap();
