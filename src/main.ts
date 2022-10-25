f422143180c73e41221d512d7709c70088540928
Rafal-wq <rafal_wilczewski@wp.pl>
2022-10-25 17:45:28 +0200

    Added cors and set it to give ability for FE communicate with this Nest app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     disableErrorMessages: true,
+  //
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //
+  //     transform: true,
+  //     transformOptions: {
+  //       enableImplicitConversion: true,
+  //     },
+  //   }),
+  // );
+
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+    }),
+  );
+  await app.listen(3001);
 }
 bootstrap();
